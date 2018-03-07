2f29a25f68e6fe54b405811ca8b8a455f5167b2b
Johann Kerr <johannkerr@gmail.com>
2018-03-06 23:21:05 -0500

    update v3.4

---
+++ v3.4/microposter/Sources/App/Models/Post.swift
@@ -0,0 +1,29 @@
+//
+//  Post.swift
+//  App
+//
+//  Created by Johann Kerr on 2/27/18.
+//
+
+import Foundation
+import Vapor
+import FluentSQLite
+
+final class Post: Content {
+    var content: String
+    var author: String
+    var id: Int?
+    
+    
+    init(content: String, author: String) {
+        
+        self.content = content
+        self.author = author
+    }
+}
+
+extension Post: SQLiteModel {}
+extension Post: Migration {}
+extension Post: Parameter {}
+
+
